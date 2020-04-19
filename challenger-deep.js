24de0529efd574fb083b26c18624cb9f26a0065d
MaxSt <max@stoerchle.at>
2020-04-19 17:00:22 +0200

    Add theme

---
+++ challenger-deep.js
@@ -0,0 +1,26 @@
+black       = '#100e23';
+red         = '#ff8080'; // red
+green       = '#95ffa4'; // green
+yellow      = '#ffe9aa'; // yellow
+blue        = '#91ddff'; // blue
+magenta     = '#c991e1'; // pink
+cyan        = '#aaffe4'; // cyan
+white       = '#cbe3e7'; // light gray
+lightBlack  = '#565575'; // medium gray
+lightRed    = '#ff5458'; // red
+lightGreen  = '#62d196'; // green
+lightYellow = '#ffb378'; // yellow
+lightBlue   = '#65b2ff'; // blue
+lightMagenta= '#906cff'; // pink
+lightCyan   = '#63f2f1'; // cyan
+lightWhite  = '#a6b3cc'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', white);
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', '#1b182c');
